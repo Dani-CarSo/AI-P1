dcf4cf6784972009385a13b7b85ff26191a2a9b9
D-CarbajalS <a23310304@live.ceti.mx>
2026-03-13 20:35:01 -0600

    Literales

---
+++ literales.py
@@ -0,0 +1,34 @@
+#LAB Modulo 2- Sección 2
+# Literales
+# Python essentials 1
+
+print("\"I'm\"\n\"\"trying\"\"\n\"\"\"learning\"\"\"Python\"\"\"")
+
+
+print("Literales")
+numero = 9
+cadena="9"
+print("literalnumero:", numero)
+print("literalcadena:", cadena)
+
+print("Numeros Octales y Hexadecimales")
+octal = 0o13
+hexadecimal = 0x16
+print("octal:", octal)
+print("hexadecimal:", hexadecimal)
+
+print("Flotantes")
+flotante = 3.14
+print("flotante:", flotante)
+
+print("Codificando flotantes")
+flotante_exponencial = 6.62607e-34
+print("flotante exponencial:", flotante_exponencial)
+
+print("Cadenas")
+print("Me gusta \"el queso y el pan\"")
+
+print("Valores booleanos")
+print(True > False)
+print(True < False)
+
