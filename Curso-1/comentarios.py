7fa68de1e47ce73124fb606d3d59bf061964cada
D-CarbajalS <a23310304@live.ceti.mx>
2026-03-13 21:18:30 -0600

    Explicacion sobre el codigo de convertir in a cm

---
+++ Curso-1/comentarios.py
@@ -0,0 +1,20 @@
+#LAB Modulo 2- Sección 5
+# Comentarios
+# Python essentials 1
+
+#Este programa habla sobre los comentarios en Python, asi que lo que esta escrito con el signo de gato o hashtag es un comentario, y no se ejecuta.
+#Los comentarios explican el codigo linea por linea
+
+#A continuacion explicare que hace cada linea del codigo del programa convertidor_simple.py
+
+#Se crean dos variables
+pulgadas = 10 #Se asigna el valor de 10 a la variable pulgadas
+centimetros = 25.4 #Se asigna el valor de 25.4 a la variable centimetros, que es la cantidad de centimetros que hay en una pulgada
+
+pulgadas_a_centimetros = pulgadas * centimetros #Se crea una nueva variable llamada pulgadas_a_centimetros, que se obtiene al multiplicar la cantidad de pulgadas por la cantidad de centimetros que hay en una pulgada
+centimetros_a_pulgadas = pulgadas_a_centimetros / centimetros #Se crea una nueva variable llamada centimetros_a_pulgadas, que se obtiene al dividir la cantidad de centimetros que hay en una pulgada por la cantidad de pulgadas que hay en una pulgada
+
+print(pulgadas, "pulgadas son", pulgadas_a_centimetros, "centímetros") #Se imprime en pantalla la cantidad de pulgadas, seguida de la frase "pulgadas son", seguida de la cantidad de centimetros que hay en esa cantidad de pulgadas, seguida de la palabra "centímetros"
+print(pulgadas_a_centimetros, "centímetros son", centimetros_a_pulgadas, "pulgadas") #Se imprime en pantalla la cantidad de centimetros que hay en esa cantidad de pulgadas, seguida de la frase "centímetros son", seguida de la cantidad de pulgadas que hay en esa cantidad de centimetros, seguida de la palabra "pulgadas"
+
+#Listo, eso es todo, espero que quedara claro, gracias
