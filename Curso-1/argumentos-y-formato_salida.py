af9f6dfbee02dd08d0a78a07a1032f17323b4729
D-CarbajalS <a23310304@live.ceti.mx>
2026-03-13 19:43:51 -0600

    Jugar con el codigo

---
+++ Curso-1/argumentos-y-formato_salida.py
@@ -0,0 +1,42 @@
+#LAB Modulo 2- Sección 1
+# Argumentos y formato de salida
+# Python essentials 1
+
+print("Carita", "Feliz", "ordenada", sep="-", end="!!!\n")
+print("con", "separador", sep="*", end=".\n")
+
+print("  *****  ")
+print(" *     * ")
+print("*  o o  *")
+print("*   ^   *")
+print("* \\___/ *")
+print(" *     * ")
+print("  *****  ")
+
+print("  *****\n *     *\n*  o o  *\n*   ^   *\n* \\___/ *\n *     *\n  *****")
+
+
+print("Mas alto:")
+
+print("    *****")
+print("   *     *")
+print("  *  o o  *")
+print(" *   ^    *")
+print("*  \\___/  *")
+print("*         *")
+print(" *       *")
+print("  *     *")
+print("   *****")
+
+
+print("doble:")
+
+print("    *****    "*2)
+print("   *     *   "*2)
+print("  *  o o  *  "*2)
+print(" *   ^    *  "*2)
+print("*  \\___/  *  "*2)
+print("*         * "*2)
+print(" *       *  "*2)
+print("  *     *   "*2)
+print("   *****    "*2)
