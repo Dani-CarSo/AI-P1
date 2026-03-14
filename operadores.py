02c2c74bebe29776a051735c56dc8acbbbb9d1f6
D-CarbajalS <a23310304@live.ceti.mx>
2026-03-13 20:47:47 -0600

    Python como calculadora

---
+++ operadores.py
@@ -0,0 +1,30 @@
+#LAB Modulo 2- Sección 3
+# Literales
+# Python essentials 1
+
+print("Suma")
+print(8+12)
+
+print("Resta, Unaria y Binaria")
+print(-4 - 4)
+print(4. - 8)
+print(-1.1)
+
+print("Multiplicación")
+print(156*7)
+
+print("División")
+print(846/8)
+
+print("División entera")
+print(846//8)
+
+print("Exponenciación")
+print(9**3.)
+print(9**3)
+print(9.**3)
+print(9.**3.)
+
+print("Módulo")
+print(78%4)
+print(9 % 6 % 2)
