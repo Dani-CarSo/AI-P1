12c6d56d59d600802ef205c8a43c17bcd08ef909
D-CarbajalS <a23310304@live.ceti.mx>
2026-03-15 12:17:04 -0600

    Queue aka

---
+++ Curso-2/fifo.py
@@ -0,0 +1,51 @@
+#LAB Modulo 3- Sección 2
+#Queue pt.1- pt.2
+# Python essentials 2
+
+class QueueError(IndexError):
+    pass
+
+class Queue:
+    def __init__(self):
+        self.queue = []
+
+    def put(self, elem):
+        self.queue.insert(0, elem)
+
+    def get(self):
+        if len(self.queue) > 0:
+            elem = self.queue[-1]
+            del self.queue[-1]
+            return elem
+        else:
+            raise QueueError
+
+class SuperQueue(Queue):
+    def isempty(self):
+        return len(self.queue) == 0
+
+que = SuperQueue()
+que.put(1)
+que.put("dog")
+que.put(False)
+for i in range(4):
+    if not que.isempty():
+        print(que.get())
+    else:
+        print("Queue empty")
+
+print("---")
+
+que = Queue()
+que.put(1)
+que.put("dog")
+que.put(False)
+try:
+    for i in range(4):
+        print(que.get())
+except:
+    print("Queue error")
+        
+
+
+    
