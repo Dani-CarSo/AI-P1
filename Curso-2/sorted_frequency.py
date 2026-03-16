d7cb813c989a8ded63a33e5219b135b45cf07ce6
D-CarbajalS <a23310304@live.ceti.mx>
2026-03-15 20:25:09 -0600

    Sorted character frequency histogram

---
+++ Curso-2/sorted_frequency.py
@@ -0,0 +1,32 @@
+#LAB Modulo 4- Sección 3
+#Sorted character frequency histogram
+# Python essentials 2
+
+from os import strerror
+
+counters = {chr(ch): 0 for ch in range(ord('a'), ord('z') + 1)}
+
+file_name = input("Enter the name of the file to analyze: ")
+
+try:
+ 
+    file = open(file_name, "rt")
+    for line in file:
+        for char in line:
+            if char.isalpha():
+                counters[char.lower()] += 1
+    file.close()
+
+
+    file = open(file_name + '.hist', 'wt')
+
+    for char in sorted(counters.keys(), key=lambda x: counters[x], reverse=True):
+        c = counters[char]
+        if c > 0:
+            file.write(char + ' -> ' + str(c) + '\n')
+
+    file.close()
+    print("Histogram saved to:", file_name + '.hist')
+
+except IOError as e:
+    print("I/O error occurred: ", strerror(e.errno))
